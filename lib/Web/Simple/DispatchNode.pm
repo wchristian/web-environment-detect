bfa343454601d018d8ae9d2f3cf9137616c79a8c
Matt S Trout <mst@shadowcat.co.uk>
2012-05-02 16:58:47 +0000

    add %_ setting

--- lib/Web/Simple/DispatchNode.pm
+++ lib/Web/Simple/DispatchNode.pm
@@ -9,7 +9,10 @@ has _app_object => (is => 'ro', init_arg => 'app_object', required => 1);
 # this ensures that the dispatchers get called as methods of the app itself
 around _curry => sub {
   my ($orig, $self) = (shift, shift);
-  $self->$orig($self->_app_object, @_);
+  my $code = $self->$orig($self->_app_object, @_);
+  ref($_[0]) eq 'HASH'
+    ? do { my $v = $_[0]; sub { local *_ = $v; &$code } }
+    : $code
 };
 
 1;
