b6863c585cd0ce5484be44f243c45f1dcf90fbfc
Matt S Trout <mst@shadowcat.co.uk>
2012-05-02 17:03:53 +0000

    expand DispatchNode comment

--- lib/Web/Simple/DispatchNode.pm
+++ lib/Web/Simple/DispatchNode.pm
@@ -7,6 +7,8 @@ extends 'Web::Dispatch::Node';
 has _app_object => (is => 'ro', init_arg => 'app_object', required => 1);
 
 # this ensures that the dispatchers get called as methods of the app itself
+# and if the first argument is a hashref, localizes %_ to it to permit
+# use of $_{name} inside the dispatch sub
 around _curry => sub {
   my ($orig, $self) = (shift, shift);
   my $code = $self->$orig($self->_app_object, @_);
